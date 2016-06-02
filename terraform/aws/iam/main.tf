b0d5e958ba13cf003243ade44b18d175eebb7dd8
enxebre <alberto.garcial@hotmail.com>
2016-06-02 13:12:52 +0100

    Edge router iam.

--- terraform/aws/iam/main.tf
+++ terraform/aws/iam/main.tf
@@ -1,3 +1,4 @@
+# master
 resource "aws_iam_role" "master_role" {
   name               = "master_role"
   path               = "/"
@@ -15,6 +16,7 @@ resource "aws_iam_instance_profile" "master_profile" {
   roles = ["${aws_iam_role.master_role.name}"]
 }
 
+# worker
 resource "aws_iam_role" "worker_role" {
   name               = "worker_role"
   path               = "/"
@@ -32,6 +34,24 @@ resource "aws_iam_instance_profile" "worker_profile" {
   roles = ["${aws_iam_role.worker_role.name}"]
 }
 
+# edge-router
+resource "aws_iam_role" "edge-router_role" {
+  name               = "edge-router_role"
+  path               = "/"
+  assume_role_policy = "${file(\"${path.module}/edge-router-role.json\")}"
+}
+
+resource "aws_iam_role_policy" "edge-router_policy" {
+  name   = "edge-router_policy"
+  role   = "${aws_iam_role.worker_role.id}"
+  policy = "${file(\"${path.module}/worker-policy.json\")}"
+}
+
+resource "aws_iam_instance_profile" "edge-router_profile" {
+  name  = "edge-router_profile"
+  roles = ["${aws_iam_role.edge-router_role.name}"]
+}
+
 # outputs
 output "master_profile_name" {
   value = "${aws_iam_instance_profile.master_profile.name}"
@@ -39,4 +59,6 @@ output "master_profile_name" {
 output "worker_profile_name" {
   value = "${aws_iam_instance_profile.worker_profile.name}"
 }
-
+output "edge-router_profile_name" {
+  value = "${aws_iam_instance_profile.edge-router_profile.name}"
+}
